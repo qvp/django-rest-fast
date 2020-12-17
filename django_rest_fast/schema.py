1a72a0f769f40419b7bc0567df752ae58f403368
Alexander Kuzmenko <bafojo@gmail.com>
2020-12-17 15:22:46 +0300

    Refactor schema implementation

--- django_rest_fast/schema.py
+++ django_rest_fast/schema.py
@@ -1,6 +1,11 @@
 from inspect import isfunction
+from typing import List, Dict
 
 from django.urls import get_resolver
+from django.forms import Form
+
+from .conf import DJANGO_REST_FAST
+from .format import method_description, method_url
 
 
 def methods_list():
@@ -11,3 +16,49 @@ def methods_list():
         if isfunction(fn) and fn.__name__ == 'drf_wrapper':
             items.append((fn, params))
     return items
+
+
+def form_params(form: Form, http_method) -> List:
+    """Get form parameters."""
+    params = []
+    for field in form():
+        params.append({
+            'in': 'query' if http_method == 'get' else 'body',
+            'name': field.name,
+            # 'required': field.required,  # fixme
+            'description': field.help_text,
+            'schema': {
+                'type': 'string'
+            }
+        })
+    return params
+
+
+def generate_schema() -> Dict:
+    """Generate swagger schema."""
+    schema = {
+        'openapi': '3.0.0',
+        'info': DJANGO_REST_FAST['info'],
+        'servers': [
+            {
+                'url': 'http://localhost:3090',  # fixme
+            },
+        ],
+        'paths': {},
+    }
+
+    for fn, params in methods_list():
+        url = method_url(params)
+
+        parameters = form_params(fn._form, fn._method) if fn._form else []
+        method_name, method_desc = method_description(fn._doc)
+        method = {
+            fn._method: {
+                'summary': method_name,
+                'description': method_desc,
+                'tags': fn._tags,
+                'parameters': parameters,
+            }
+        }
+        schema['paths'][url] = method
+    return schema
--- django_rest_fast/views.py
+++ django_rest_fast/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 from .conf import DJANGO_REST_FAST
-from .format import method_description, method_url
-from .schema import methods_list
+from .schema import methods_list, form_params, generate_schema
 
 
 def docs(request):
@@ -11,42 +10,5 @@ def docs(request):
 
 
 def schema(request):
-    data = {
-        'openapi': '3.0.0',
-        'info': DJANGO_REST_FAST['info'],
-        'servers': [
-            {
-                'url': 'http://localhost:3090',
-            },
-        ],
-        'paths': {},
-    }
-
-    for fn, params in methods_list():
-        url = method_url(params)
-        
-        parameters = None
-        if fn._form:
-            parameters = []
-            for field in fn._form():
-                parameters.append({
-                    'in': 'query' if fn._method == 'get' else 'body',
-                    'name': field.name,
-                    # 'required': field.required,
-                    'description': field.help_text,
-                    'schema': {
-                        'type': 'string'
-                    }
-                })
-
-        method_name, method_desc = method_description(fn._doc)
-        method = {
-            fn._method: {
-                'summary': method_name,
-                'description': method_desc,
-                'tags': fn._tags,
-                'parameters': parameters,
-            }
-        }
-        data['paths'][url] = method
-    return JsonResponse(data)
+    schema_dict = generate_schema()
+    return JsonResponse(schema_dict)
