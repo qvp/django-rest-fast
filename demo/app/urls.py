56fc1eb5288b24a2ce930fc6efbfbd2e2cf9f88a
Alexander Kuzmenko <bafojo@gmail.com>
2020-12-23 17:14:31 +0300

    create stubs for blog methods

--- demo/app/urls.py
+++ demo/app/urls.py
@@ -3,8 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.posts),
-    path('posts/create', views.posts_create),
     path('users/login', views.users_login),
     path('users/registration', views.users_registration),
+
+    path('posts', views.posts),
+    path('posts/create', views.posts_create),
+    path('posts/update', views.posts_update),
+    path('posts/update_partial', views.posts_update_partial),
 ]
--- demo/app/views.py
+++ demo/app/views.py
@@ -1,5 +1,5 @@
 from django.http.response import JsonResponse
-from django_rest_fast.decorators import get, post
+from django_rest_fast.decorators import get, post, put, patch, delete
 
 from .forms import UserLoginForm, UserRegistrationForm
 
@@ -35,3 +35,36 @@ def posts_create(request):
     # if request.method == 'POST' and form.is_valid():
     #     print('OK!')
     return JsonResponse({'request_data': request.POST})
+
+
+@put(tags=['blog'])
+def posts_update(request):
+    """
+    Update post.
+    """
+    # form = BarForm(data=request.body)
+    # if request.method == 'POST' and form.is_valid():
+    #     print('OK!')
+    return JsonResponse({'request_data': request.body})
+
+
+@patch(tags=['blog'])
+def posts_update_partial(request):
+    """
+    Update post partial.
+    """
+    # form = BarForm(data=request.body)
+    # if request.method == 'POST' and form.is_valid():
+    #     print('OK!')
+    return JsonResponse({'request_data': request.body})
+
+
+@delete(tags=['blog'])
+def posts_delete(request):
+    """
+    Delete post.
+    """
+    # form = BarForm(data=request.body)
+    # if request.method == 'POST' and form.is_valid():
+    #     print('OK!')
+    return JsonResponse({'request_data': request.body})
